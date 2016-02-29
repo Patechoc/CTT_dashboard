f5d0820f3aae3cdbae1a592d82d0fdba868674eb
hanshenrik <hh.gronsleth@gmail.com>
2016-03-01 00:00:43 +0100

    Add fullscreen plugin to regional map

--- js/regionListener.js
+++ js/regionListener.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById("regionButton").addEventListener("click", displayRegionMap);
-    });
+});
 
 
-function displayRegionMap(){
+function displayRegionMap() {
     document.getElementById("map").style.display = "none";
     document.getElementById("regionMap").style.display = "block";
     
@@ -12,5 +12,5 @@ function displayRegionMap(){
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>', maxZoom: 18, id: 'mapbox.emerald',
                 accessToken: 'pk.eyJ1IjoiZnJlZHJpa2FudGhvbmlzZW4iLCJhIjoiY2lrcXk1cTJzMDAxM3dsa3FmOWRiejJtNyJ9.HMZmGTfUijbKOxKt7P78rQ'}).addTo(map);
     
-   
+   L.control.fullscreen().addTo(map);
 }
